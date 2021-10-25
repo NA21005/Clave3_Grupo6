e0ac1cf953c5f2102a22d3c0664cae7ebe50bbae
Emerson Alexander Cueva Carbajal <CC21045@ti.ues.edu.sv>
2021-10-24 18:55:56 -0600

    Formulario de Inicio de sesion (Cambio 1)

--- Clave3_Grupo6/Clave3_Grupo6/Form1.Designer.cs
+++ Clave3_Grupo6/Clave3_Grupo6/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Clave3_Grupo6
 {
-    partial class Form1
+    partial class iniciarSecion
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(iniciarSecion));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Contraseña = new System.Windows.Forms.TextBox();
+            this.Usuario = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.inicioSesion = new System.Windows.Forms.Button();
+            this.Salir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(79)))), ((int)(((byte)(183)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(441, 367);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(560, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Inicio de Sesion";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(494, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Usuario:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(494, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Contraseña:";
+            // 
+            // Contraseña
+            // 
+            this.Contraseña.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(96)))), ((int)(((byte)(138)))));
+            this.Contraseña.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Contraseña.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Contraseña.ForeColor = System.Drawing.SystemColors.Info;
+            this.Contraseña.Location = new System.Drawing.Point(22, 14);
+            this.Contraseña.Name = "Contraseña";
+            this.Contraseña.PasswordChar = '*';
+            this.Contraseña.Size = new System.Drawing.Size(142, 19);
+            this.Contraseña.TabIndex = 4;
+            this.Contraseña.TextChanged += new System.EventHandler(this.Contraseña_TextChanged);
+            // 
+            // Usuario
+            // 
+            this.Usuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(96)))), ((int)(((byte)(138)))));
+            this.Usuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Usuario.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Usuario.Location = new System.Drawing.Point(22, 14);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(142, 19);
+            this.Usuario.TabIndex = 5;
+            this.Usuario.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(96)))), ((int)(((byte)(138)))));
+            this.panel1.Controls.Add(this.Usuario);
+            this.panel1.Location = new System.Drawing.Point(498, 139);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 46);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(96)))), ((int)(((byte)(138)))));
+            this.panel2.Controls.Add(this.Contraseña);
+            this.panel2.Location = new System.Drawing.Point(498, 268);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(180, 45);
+            this.panel2.TabIndex = 7;
+            // 
+            // inicioSesion
+            // 
+            this.inicioSesion.BackColor = System.Drawing.Color.Transparent;
+            this.inicioSesion.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.inicioSesion.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inicioSesion.Location = new System.Drawing.Point(498, 362);
+            this.inicioSesion.Name = "inicioSesion";
+            this.inicioSesion.Size = new System.Drawing.Size(97, 23);
+            this.inicioSesion.TabIndex = 8;
+            this.inicioSesion.Text = "Iniciar sesion";
+            this.inicioSesion.UseVisualStyleBackColor = false;
+            this.inicioSesion.Click += new System.EventHandler(this.inicioSesion_Click);
+            // 
+            // Salir
+            // 
+            this.Salir.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Salir.Location = new System.Drawing.Point(650, 362);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(99, 23);
+            this.Salir.TabIndex = 9;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Click += new System.EventHandler(this.Salir_Click);
+            // 
+            // iniciarSecion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(113)))), ((int)(((byte)(79)))), ((int)(((byte)(183)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.inicioSesion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "iniciarSecion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Contraseña;
+        private System.Windows.Forms.TextBox Usuario;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button inicioSesion;
+        private System.Windows.Forms.Button Salir;
     }
 }
 
--- Clave3_Grupo6/Clave3_Grupo6/Form1.cs
+++ Clave3_Grupo6/Clave3_Grupo6/Form1.cs
@@ -10,11 +10,56 @@ using System.Windows.Forms;
 
 namespace Clave3_Grupo6
 {
-    public partial class Form1 : Form
+    public partial class iniciarSecion : Form
     {
-        public Form1()
+        public iniciarSecion()
         {
             InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Contraseña_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void inicioSesion_Click(object sender, EventArgs e)
+        {
+            //Declaracion de variables
+            Form Formulario1 = new iniciarSecion();
+            Form Formulario2 = new Seleccion();
+            string Cuenta, Contra;
+
+            Cuenta = Convert.ToString(Usuario.Text);
+            Contra = Convert.ToString(Contraseña.Text);
+
+            // Validar si el usuario y contraseña son Correctos
+            if (Cuenta == "prn115")
+            {
+                if (Contra == "prn115")
+                {
+                    Formulario1.Close();
+                    Formulario2.Show();
+                }
+                else
+                {
+                    MessageBox.Show("La contraseña es incorrecta");
+                }
+            }
+            else
+            {
+                MessageBox.Show("La cuenta es incorrecta");
+            }
+
         }
     }
 }
